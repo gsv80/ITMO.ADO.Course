f19f4abf9d837f8a508da6f29bfd715982024248
Sergey Gorbanev <gsv80@yandex.ru>
2023-01-28 01:04:48 +0300

    lab7 - Linq practice added

---
+++ task7/task7_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static task7_1.Program;
+
+namespace task7_1
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int Id { get; set; }
+            public List<int> Scores;
+
+        }
+
+        static List<Student> students = new List<Student>
+        {
+           new Student {First="Svetlana", Last="Omelchenko", Id=111, Scores= new List<int> {97, 92, 81, 60}
+        },
+           new Student { First = "Claire", Last = "O’Donnell", Id = 112, Scores = new List<int> { 75, 84, 91, 39 } },
+        };
+
+    static void Main(string[] args)
+            {
+                IEnumerable<Student> studentQuery =
+                from student in students
+                where student.Scores[0] > 90
+                select student;
+
+                foreach (Student student in studentQuery)
+                {
+                    Console.WriteLine("{0}, {1}", student.Last, student.First);
+                }
+            }
+        }
+    }
---
+++ task7/task7_3/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static task7_3.Program;
+
+namespace task7_3
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string First { get; set; }
+            public string Last { get; set; }
+            public int Id { get; set; }
+            public List<int> Scores;
+
+        }
+
+        static List<Student> students = new List<Student>
+        {
+           new Student {First="Svetlana", Last="Omelchenko", Id=111, Scores= new List<int> {97, 92, 81, 60}
+        },
+           new Student { First = "Claire", Last = "O’Donnell", Id = 112, Scores = new List<int> { 75, 84, 91, 39 } },
+        };
+
+        static void Main(string[] args)
+        {
+            IEnumerable<Student> studentQuery =
+            from student in students
+            where student.Scores[0] > 70 && student.Scores[3] < 80
+            orderby student.Scores[0] descending
+            select student;
+
+            Console.WriteLine("Query1");
+            foreach (Student student in studentQuery)
+            {
+                Console.WriteLine(" {0}, {1} {2}", student.Last, student.First, student.Scores[0]);
+            }
+
+            var studentQuery2 =
+            from student in students
+            group student by student.Last[0];
+
+            Console.WriteLine("Query2");
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+                foreach (Student student in studentGroup)
+                {
+                    Console.WriteLine("   {0}, {1}",
+                                student.Last, student.First);
+                }
+            }
+
+
+            var studentQuery3 =
+            from student in students
+            group student by student.Last[0];
+
+            Console.WriteLine("Query3");
+            foreach (var groupOfStudents in studentQuery3)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("  {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+
+            var studentQuery4 =
+                from student in students
+                group student by student.Last[0] into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+
+            Console.WriteLine("Query4");
+            foreach (var groupOfStudents in studentQuery4)
+            {
+                Console.WriteLine(groupOfStudents.Key);
+                foreach (var student in groupOfStudents)
+                {
+                    Console.WriteLine("  {0}, {1}",
+                        student.Last, student.First);
+                }
+            }
+
+            var studentQuery5 =
+            from student in students
+            let totalScore = student.Scores[0] + student.Scores[1] + student.Scores[2] + student.Scores[3]
+            where totalScore / 4 < student.Scores[0]
+            select student.Last + " " + student.First;
+
+            Console.WriteLine("Query5");
+
+            foreach (string s in studentQuery5)
+            {
+                Console.WriteLine(s);
+            }
+
+            var studentQuery6 =
+            from student in students
+            let totalScore = student.Scores[0] + student.Scores[1] +
+                student.Scores[2] + student.Scores[3]
+            select totalScore;
+
+            double averageScore = studentQuery6.Average();
+            Console.WriteLine("Query6");
+
+            Console.WriteLine("Class average score = {0}", averageScore);
+
+
+            IEnumerable<string> studentQuery7 =
+                from student in students
+                where student.Last == "Garcia"
+                select student.First;
+
+
+                Console.WriteLine("Query7");
+                Console.WriteLine("The Garcias in the class are:");
+                foreach (string s in studentQuery7)
+                {
+                    Console.WriteLine(s);
+                }
+
+            var studentQuery8 =
+                from student in students
+                let x = student.Scores[0] + student.Scores[1] +
+                    student.Scores[2] + student.Scores[3]
+                where x > averageScore
+                select new { id = student.Id, score = x };
+            Console.WriteLine("Query8");
+
+            foreach (var item in studentQuery8)
+            {
+                Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
+            }
+
+
+
+        }
+    }
+}
