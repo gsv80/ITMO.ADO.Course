8fb178e776ffc9c5d70f67b8f696abd0466bef82
Sergey Gorbanev <gsv80@ya.com>
2023-02-05 09:34:58 +0200

    testTask foms added

---
+++ ADO.TestTask/AuthForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ADO.TestTask
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonReject = new System.Windows.Forms.Button();
+            this.labelHelp = new System.Windows.Forms.Label();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(163, 197);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(87, 27);
+            this.buttonEnter.TabIndex = 0;
+            this.buttonEnter.Text = "Вход";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonReject
+            // 
+            this.buttonReject.Location = new System.Drawing.Point(163, 231);
+            this.buttonReject.Name = "buttonReject";
+            this.buttonReject.Size = new System.Drawing.Size(87, 27);
+            this.buttonReject.TabIndex = 1;
+            this.buttonReject.Text = "Отмена";
+            this.buttonReject.UseVisualStyleBackColor = true;
+            this.buttonReject.Click += new System.EventHandler(this.buttonReject_Click);
+            // 
+            // labelHelp
+            // 
+            this.labelHelp.AutoSize = true;
+            this.labelHelp.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHelp.Location = new System.Drawing.Point(78, 19);
+            this.labelHelp.Name = "labelHelp";
+            this.labelHelp.Size = new System.Drawing.Size(261, 21);
+            this.labelHelp.TabIndex = 2;
+            this.labelHelp.Text = "Для входа введите Логин и Пароль";
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(44, 72);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(41, 15);
+            this.labelLogin.TabIndex = 3;
+            this.labelLogin.Text = "Логин";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(44, 121);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(49, 15);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Пароль";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(135, 68);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(142, 23);
+            this.textBoxLogin.TabIndex = 5;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(135, 118);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(142, 23);
+            this.textBoxPassword.TabIndex = 6;
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 290);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.labelHelp);
+            this.Controls.Add(this.buttonReject);
+            this.Controls.Add(this.buttonEnter);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "AuthForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonReject;
+        private System.Windows.Forms.Label labelHelp;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ ADO.TestTask/AuthForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO.TestTask
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonReject_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            string login = textBoxLogin.Text;
+            string pas = textBoxPassword.Text;
+
+            
+            adminForm aForm = new adminForm();
+            
+            aForm.Show();
+            this.Hide();
+        }
+    }
+}
